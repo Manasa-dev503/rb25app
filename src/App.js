961080d3f104a6b4b10a5b75f46551622f7a061c
Manasa-dev503 <198a1a0503manasa@gmail.com>
2023-09-01 21:59:22 +0530

    Review

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Reviews from './Review';
 
 function App() {
   return (
     <div className="mybox">
       <h1>ReactJS</h1>
+      <Reviews></Reviews>
       
     </div>
   );
