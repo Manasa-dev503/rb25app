441d13e709980fdace672b01d2aa54e168d6d627
Manasa-dev503 <198a1a0503manasa@gmail.com>
2023-09-01 23:20:52 +0530

    vacation

--- src/Vacationplan.jsx
+++ src/Vacationplan.jsx
@@ -1,49 +1,49 @@
 import React from "react";
 import axios from 'axios';
 
-function Vacation(){
-    var [x,setx]=React.useState([""])
+function Project(){
+    var [tour,setTour]=React.useState([])
     React.useEffect(()=>{
         axios.get("https://course-api.com/react-tours-project")
         .then((res)=>{
-            console.log(res.data)
-            var x = res.data.map((y)=>{
+           
+          var tour = res.data.map((y)=>{
                 return {...y,flag:false}
-
             })
-            setx([...x])
+            // console.log(res.data)
+             setTour([...tour])
         })
     },[])
-    function del(i){
-        var temp=[...x]
+    function notintrested(i){
+        var temp=[...tour]
         temp.splice(i,1)
-        setx([...temp])
+        setTour([...temp])
     }
-    function abc(i){
-        var temp = [...x]
-        temp[i].flag=!temp[i].flag
-        setx([...temp])
-    }   
+    function readmore(ind){
+       var temp=([...tour])
+       temp[ind].flag =! temp[ind].flag
+       setTour([...temp])
+    }
     return(
         <div>
         <center>
             <h1>Our Tours</h1>
             </center>
             {
-        x.map((a,i)=>{
+        tour.map((place,i)=>{
             return <div>
             <center>
             <div class="card" style={{width:"40%",height:"auto",marginBottom:"30px"}}>
-             <img src={a.image} style={{width:"100%",padding:"5px",height:"100%"}} alt="" />
+             <img src={place.image} style={{width:"100%",padding:"5px"}} alt="" />
              <div class="card-body" style={{width:"auto",padding:"5px"}}>
-                 <span><p class="text-start" style={{fontSize:"20px"}}><b>{a.name}</b></p><p class="text-end"><span class="badge bg-secondary">{"$"+ a.price}</span></p></span>
-                 <p>{a.flag?a.info:a.info.slice(0,200)}</p>
-                 <button onClick={()=>{abc(i)}}>Read More</button>
-                 <button onClick={()=>{del(i)}} style={{borderColor:"red",color:"red"}}>Not intrested</button>
+                 <span><p class="text-start" style={{fontSize:"20px"}}><b>{place.name}</b></p><p class="text-end"><span class="badge bg-secondary">{place.price}</span></p></span>
+                 <p>{place.flag?place.info:place.info.slice(0,200)}
+                 <button onClick={()=>{readmore(i)}}>Read more</button></p>
+                 <button onClick={()=>{notintrested(i)}} style={{borderColor:"red",color:"red"}}>Not intrested</button>
              </div>
              </div>
             </center>    
-            </div>
+              </div>
         })
     }
         </div>
@@ -51,4 +51,4 @@ function Vacation(){
     )
    
 }
-export default Vacation;
+export default Project;
