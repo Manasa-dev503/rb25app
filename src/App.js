ff5e64e93b09b9e63f8831a73a51367dbbc5c6a9
Manasa-dev503 <198a1a0503manasa@gmail.com>
2023-09-01 21:48:30 +0530

    vacation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Vacation from './Vacationplan';
 
 function App() {
   return (
     <div className="mybox">
       <h1>ReactJS</h1>
+      <Vacation></Vacation>
       
     </div>
   );
