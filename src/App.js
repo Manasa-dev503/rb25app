4195ca1a4c91a5800247fcfd776fe5cfdcded51a
Manasa-dev503 <198a1a0503manasa@gmail.com>
2023-09-01 19:59:13 +0530

    accordion

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Accordion from './Accordion1';
 
 function App() {
   return (
     <div className="mybox">
-      <h1>ReactJS</h1>
+      <Accordion></Accordion>
       
     </div>
   );
