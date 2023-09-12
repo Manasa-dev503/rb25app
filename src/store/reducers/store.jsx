2aac6baaba0d5a2c47c3379d5be8ab58318fa8a6
Manasa-dev503 <198a1a0503manasa@gmail.com>
2023-09-12 10:30:02 +0530

    Redux22

--- src/store/reducers/store.jsx
+++ src/store/reducers/store.jsx
@@ -3,4 +3,4 @@ import CounterReducer from './CounterReducer';
 import TodoReducer from './TodoReducer';
 const reducer = combineReducers({c:CounterReducer,t:TodoReducer})
 const store = new createStore(reducer);
-export default store;
+export default store;      
